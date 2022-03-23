2de83dafb3a80f57995eb2d686dd6678b800b270
weilei <wlleiwei@163.com>
2022-03-23 22:59:49 +0800

    AbstractZuulFilter 搭建

---
+++ coupon-gateway/src/main/java/com/imooc/coupon/filter/abstractFilter/AbstractPostFilter.java
@@ -0,0 +1,35 @@
+package com.imooc.coupon.filter.abstractFilter;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+/**
+ * <h1>post 抽象后置过滤器</h1>
+ *
+ * @author 魏磊
+ */
+public abstract class AbstractPostFilter extends AbstractZuulFilter {
+
+    /**
+     * to classify a filter by type. Standard types in Zuul are "pre" for pre-routing filtering,
+     * "route" for routing to an origin, "post" for post-routing filters, "error" for error handling.
+     * We also support a "static" type for static responses see  StaticResponseFilter.
+     * Any filterType made be created or added and run by calling FilterProcessor.runFilters(type)
+     *
+     * @return A String representing that type
+     */
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    /**
+     * filterOrder() must also be defined for a filter. Filters may have the same  filterOrder if precedence is not
+     * important for a filter. filterOrders do not need to be sequential.
+     *
+     * @return the int order of a filter
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+}
---
+++ coupon-gateway/src/main/java/com/imooc/coupon/filter/abstractFilter/AbstractZuulFilter.java
@@ -0,0 +1,78 @@
+package com.imooc.coupon.filter.abstractFilter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+/**
+ * <h1> 自定义Zuul网关抽象过滤器，具体的过滤器实现继承此类 </h1>
+ *
+ * @author 魏磊
+ */
+
+public abstract class AbstractZuulFilter extends ZuulFilter {
+
+    /**
+     * 用于过滤器之间传递消息，数据保存在每个请求的ThreadLocal中
+     */
+    RequestContext requestContext;
+    /**
+     * 用于判断当前过滤器是否执行
+     */
+    private final static String NEXT = "next";
+
+    /**
+     * a "true" return from this method means that the run() method should be invoked
+     *
+     * @return true if the run() method should be invoked. false will not invoke the run() method
+     */
+    @Override
+    public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        return (boolean) requestContext.getOrDefault(NEXT, true);
+    }
+
+    /**
+     * if shouldFilter() is true, this method will be invoked. this method is the core method of a ZuulFilter
+     *
+     * @return Some arbitrary artifact may be returned. Current implementation ignores it.
+     * @throws ZuulException if an error occurs during execution.
+     */
+    @Override
+    public Object run() throws ZuulException {
+        requestContext = RequestContext.getCurrentContext();
+        return execute();
+    }
+
+    /**
+     * 具体过滤器来实现执行逻辑
+     *
+     * @return 执行结果
+     */
+    protected abstract Object execute();
+
+
+    /**
+     * 执行时失败，直接返回，不执行后续的过滤器
+     *
+     * @param code 状态码
+     * @param msg  错误信息
+     */
+    void fail(int code, String msg) {
+        //后续的过滤器不执行
+        requestContext.set(NEXT, false);
+        //直接返回
+        requestContext.setSendZuulResponse(false);
+        requestContext.getResponse().setContentType("text/html:charset=UTF-8");
+        requestContext.setResponseStatusCode(code);
+        requestContext.setResponseBody(String.format("{\"result\":\"%s\"}", msg));
+    }
+
+
+    /**
+     * 执行成功设置NEXT=TRUE，执行后续的过滤器
+     */
+    void success() {
+        requestContext.set(NEXT, true);
+    }
+}
